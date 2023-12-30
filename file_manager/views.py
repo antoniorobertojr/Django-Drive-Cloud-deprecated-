fae88311c5d0474d96f178ef3bce76b321dd781a
Antonio Roberto <juniormach96@gmail.com>
2023-12-30 05:48:48 -0500

    add share and unshare views to folder model

--- file_manager/views.py
+++ file_manager/views.py
@@ -43,3 +43,48 @@ class FolderDeleteView(generics.DestroyAPIView):
     permission_classes = [IsOwner]
     queryset = Folder.objects.all()
     serializer_class = FolderSerializer
+
+
+class ShareFolderView(APIView):
+    permission_classes = [IsOwner]
+
+    def post(self, request, folder_id):
+        folder = get_object_or_404(Folder, id=folder_id)
+
+        # Manually check object permissions
+        self.check_object_permissions(request, folder)
+
+        user_to_share_with = User.objects.get(username=request.data.get("username"))
+        folder.shared_with.add(user_to_share_with)
+        return Response({"status": "folder shared"}, status=status.HTTP_200_OK)
+
+
+class UnshareFolderView(APIView):
+    permission_classes = [IsOwner]
+
+    def post(self, request, folder_id):
+        folder = get_object_or_404(Folder, id=folder_id)
+        self.check_object_permissions(request, folder)
+
+        user_to_unshare_with = User.objects.get(username=request.data.get("username"))
+        folder.shared_with.remove(user_to_unshare_with)
+        return Response({"status": "folder unshared"}, status=status.HTTP_200_OK)
+
+
+# Common
+# class SearchView(generics.GenericAPIView):
+#     def get(self, request, *args, **kwargs):
+#         query = request.query_params.get("q", "")
+
+#         # Search in Files
+#         files = File.objects.filter(name__icontains=query)
+#         file_serializer = FileSerializer(files, many=True)
+
+#         # Search in Folders
+#         folders = Folder.objects.filter(name__icontains=query)
+#         folder_serializer = FolderSerializer(folders, many=True)
+
+#         return Response(
+#             {"files": file_serializer.data, "folders": folder_serializer.data},
+#             status=status.HTTP_200_OK,
+#         )
