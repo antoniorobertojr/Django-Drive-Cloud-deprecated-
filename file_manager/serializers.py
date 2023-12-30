7139d15a7cafe41abaf6319bb0855a841054dcff
Antonio Roberto <juniormach96@gmail.com>
2023-12-30 17:00:02 -0500

    implement recursivefield on serializer

--- file_manager/serializers.py
+++ file_manager/serializers.py
@@ -3,8 +3,14 @@ from rest_framework import serializers
 from .models import Folder
 
 
+class RecursiveField(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = FolderSerializer(value, context=self.context)
+        return serializer.data
+
 class FolderSerializer(serializers.ModelSerializer):
+    children = serializers.ListSerializer(child=RecursiveField(), required=False)
+
     class Meta:
         model = Folder
-        fields = ['id', 'name', 'text_color', 'parent', 'created_at', 'updated_at']
-
+        fields = ['id', 'name', 'parent', 'children']
