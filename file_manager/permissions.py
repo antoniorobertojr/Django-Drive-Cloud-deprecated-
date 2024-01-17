12938a332a959e6f78f284d51e3692bb7eb91338
Antonio Roberto <juniormach96@gmail.com>
2024-01-17 12:36:18 -0500

    add isowner and currentUserCanRead permissions

--- file_manager/permissions.py
+++ file_manager/permissions.py
@@ -1,19 +1,35 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsAuthenticated
+
+from .models import ContentType, File, Folder, Share
 
 
-class IsOwnerOrSharedWith(BasePermission):
+class IsOwner(IsAuthenticated):
     message = "You must be the owner of this folder or it must be shared with you to access it."
 
-    def has_object_permission(self, request, view, obj):
-        if obj.user == request.user:
-            return True
-
-        return request.user in obj.shared_with.all()
-
-
-class IsOwner(BasePermission):
-    message = "You must be the owner to perform this action on this object."
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        return obj.owner == request.user
 
+
+class CurrentUserCanRead(IsAuthenticated):
+    message = "Access to this item is restricted."
+
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, Folder):
+            content_type = ContentType.objects.get_for_model(Folder)
+        elif isinstance(obj, File):
+            content_type = ContentType.objects.get_for_model(File)
+        else:
+            return False
+
+        shared_access = Share.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            shared_with=request.user,
+            can_read=True  # Assuming we are checking for read permission
+        ).exists()
+
+        # Grant permission if the object is shared with the current user with read access
+        return shared_access
