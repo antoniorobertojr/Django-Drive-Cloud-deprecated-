753329471d095e4ddbe958c8508b4299fea1eda2
Antonio Roberto <juniormach96@gmail.com>
2024-01-17 13:09:31 -0500

    abstract currentUser logic and create more permissions

--- file_manager/permissions.py
+++ file_manager/permissions.py
@@ -13,23 +13,66 @@ class IsOwner(IsAuthenticated):
         return obj.owner == request.user
 
 
-class CurrentUserCanRead(IsAuthenticated):
-    message = "Access to this item is restricted."
+class CurrentUserCanAccess(IsAuthenticated):
+    def _get_content_type(self, obj):
+        if isinstance(obj, Folder):
+            return ContentType.objects.get_for_model(Folder)
+        elif isinstance(obj, File):
+            return ContentType.objects.get_for_model(File)
+        else:
+            return None
 
     def has_object_permission(self, request, view, obj):
-        if isinstance(obj, Folder):
-            content_type = ContentType.objects.get_for_model(Folder)
-        elif isinstance(obj, File):
-            content_type = ContentType.objects.get_for_model(File)
-        else:
+        content_type = self._get_content_type(obj)
+        if not content_type:
             return False
 
-        shared_access = Share.objects.filter(
+        return self.check_permission(request, obj, content_type)
+
+    def check_permission(self, request, obj, content_type):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class CurrentUserCanRead(CurrentUserCanAccess):
+    message = "You do not have read permissions for this object."
+
+    def check_permission(self, request, obj, content_type):
+        return Share.objects.filter(
             content_type=content_type,
             object_id=obj.id,
             shared_with=request.user,
-            can_read=True  # Assuming we are checking for read permission
+            can_read=True
         ).exists()
 
-        # Grant permission if the object is shared with the current user with read access
-        return shared_access
+class CurrentUserCanEdit(CurrentUserCanAccess):
+    message = "You do not have edit permissions for this object."
+
+    def check_permission(self, request, obj, content_type):
+        return Share.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            shared_with=request.user,
+            can_edit=True
+        ).exists()
+
+class CurrentUserCanShare(CurrentUserCanAccess):
+    message = "You do not have share permissions for this object."
+
+    def check_permission(self, request, obj, content_type):
+        return Share.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            shared_with=request.user,
+            can_share=True
+        ).exists()
+
+class CurrentUserCanDelete(CurrentUserCanAccess):
+    message = "You do not have delete permissions for this object."
+
+    def check_permission(self, request, obj, content_type):
+        return Share.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            shared_with=request.user,
+            can_delete=True
+        ).exists()
