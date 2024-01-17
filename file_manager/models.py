0e831c6d627bdb5069e12410d26e184313c0fa0d
Antonio Roberto <juniormach96@gmail.com>
2024-01-17 12:35:50 -0500

    add share model

--- file_manager/models.py
+++ file_manager/models.py
@@ -1,18 +1,21 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.urls import reverse
 
+User = get_user_model()
 
-# TODO get_user() instead of auth user model.
+
 class Folder(models.Model):
     name = models.CharField(max_length=128)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="owned_folders"
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="owned_folders"
     )
-    shared_with = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="shared_folders", blank=True
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
     )
-    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
 
@@ -38,25 +41,54 @@ class Folder(models.Model):
         return url_list
 
 
+class Share(models.Model):
+    shared_by = models.ForeignKey(User, related_name='shares_made', on_delete=models.CASCADE)
+    shared_with = models.ForeignKey(User, related_name='shares_received', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    can_read = models.BooleanField(default=True)
+    can_edit = models.BooleanField(default=False)
+    can_delete = models.BooleanField(default=False)
+    can_share = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('content_type', 'object_id', 'shared_with')
+
+    def __str__(self):
+        return f"{self.user} -> {self.content_object}"
+
+
 class File(models.Model):
     name = models.CharField(max_length=128)
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True, blank=True)
-    file = models.FileField(upload_to='documents/')
+    file = models.FileField(upload_to="documents/")
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="owned_files"
+    )
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
         if not self.name:
             self.name = self.file.name
-        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+        return super().save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields,
+        )
 
     @property
     def full_path(self):
-        return f'{self.folder}::{self.name}'
+        return f"{self.folder}::{self.name}"
 
     @property
     def extension(self):
-        return str(self.name).split('.')[-1]
+        return str(self.name).split(".")[-1]
