11ff5c999b0dc5f7eddf39b9d841a94e3bbad97f
Antonio Roberto <juniormach96@gmail.com>
2024-01-18 14:26:16 -0500

    abstracto common logic into another function

--- file_manager/signals.py
+++ file_manager/signals.py
@@ -5,27 +5,35 @@ from django.dispatch import receiver
 from .models import File, Folder, Share
 
 
+def copy_permissions(
+    source_content_type, source_object_id, target_content_type, target_object_id
+):
+    shares = Share.objects.filter(
+        content_type=source_content_type, object_id=source_object_id
+    )
+
+    for share in shares:
+        Share.objects.create(
+            shared_by=share.shared_by,
+            shared_with=share.shared_with,
+            content_type=target_content_type,
+            object_id=target_object_id,
+            can_read=share.can_read,
+            can_edit=share.can_edit,
+            can_delete=share.can_delete,
+            can_share=share.can_share,
+        )
+
+
 @receiver(post_save, sender=File)
 def copy_folder_permissions_to_file(sender, instance, created, **kwargs):
     if created and instance.folder:
         folder_content_type = ContentType.objects.get_for_model(Folder)
         file_content_type = ContentType.objects.get_for_model(File)
 
-        # Copy the permissions from the Folder to the File
-        folder_shares = Share.objects.filter(
-            content_type=folder_content_type, object_id=instance.folder.id
+        copy_permissions(
+            folder_content_type, instance.folder.id, file_content_type, instance.id
         )
-        for share in folder_shares:
-            Share.objects.create(
-                shared_by=share.shared_by,
-                shared_with=share.shared_with,
-                content_type=file_content_type,
-                object_id=instance.id,
-                can_read=share.can_read,
-                can_edit=share.can_edit,
-                can_delete=share.can_delete,
-                can_share=share.can_share,
-            )
 
 
 @receiver(post_save, sender=Folder)
@@ -33,17 +41,6 @@ def copy_parent_folder_permissions_to_subfolder(sender, instance, created, **kwa
     if created and instance.parent:
         folder_content_type = ContentType.objects.get_for_model(Folder)
 
-        folder_shares = Share.objects.filter(
-            content_type=folder_content_type, object_id=instance.parent.id
+        copy_permissions(
+            folder_content_type, instance.parent.id, folder_content_type, instance.id
         )
-        for share in folder_shares:
-            Share.objects.create(
-                shared_by=share.shared_by,
-                shared_with=share.shared_with,
-                content_type=folder_content_type,
-                object_id=instance.id,
-                can_read=share.can_read,
-                can_edit=share.can_edit,
-                can_delete=share.can_delete,
-                can_share=share.can_share,
-            )
